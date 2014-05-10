5056a86c9abb6de6ba6ed06a7b0fced1dbbb04fa
jbowling <jess.a.bowling@gmail.com>
2014-05-09 22:51:34 -0400

    project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Porto::Application.config.secret_key_base = 'c9bbd096f50a0160211b254f8855cf5bb042f9af595b98531ff1023172950f1be5aacb8fd8e4faa91ca1cbb2e6940b568b751e166f855dcbe431265772f947ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Porto::Application.config.session_store :cookie_store, key: '_porto_session'
